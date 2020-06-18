9bc3e4d04dd9815028ab3305ab5718f627e1d7f7
jaem1n207 <jaem1n207@naver.com>
2020-06-18 17:31:14 +0900

    [ADD] tslint & prettier

---
+++ worktogether/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>우리 함께 노래합시다~</code> and save to reload.
+        </p>
+        Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
